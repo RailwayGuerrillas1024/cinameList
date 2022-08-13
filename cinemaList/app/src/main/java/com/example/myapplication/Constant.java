6fef934be6a6fcb20fe926886120c82b7c21bcbf
1454483114@qq.com <1454483114@qq.com>
2022-08-13 23:04:10 +0800

    cinemaList

---
+++ cinemaList/app/src/main/java/com/example/myapplication/Constant.java
@@ -0,0 +1,6 @@
+package com.example.myapplication;
+
+public class Constant {
+    public  static final String get_url="";
+
+}
---
+++ cinemaList/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import android.app.DownloadManager.Request;
+
+
+import android.app.DownloadManager;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.telecom.Call;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.textclassifier.TextLinks;
+import android.widget.Button;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import javax.security.auth.callback.Callback;
+
+import okhttp3.OkHttpClient;
+
+public class MainActivity extends AppCompatActivity {
+    private ViewPager loopPager;
+    private loopPagerAdapter loopPagerAdapter;
+    private static List<Integer> sPics=new ArrayList<>();
+    private Handler handler;
+
+
+    static{
+        sPics.add(R.mipmap.t1);
+        sPics.add(R.mipmap.t2);
+        sPics.add(R.mipmap.t3);
+        }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+        initView();
+
+        handler=new Handler();
+
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, MainActivity3.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        //当我这个界面绑定到窗口
+        handler.post(loopTask);
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        handler.removeCallbacks(loopTask);
+    }
+
+    private Runnable loopTask=new Runnable() {
+        @Override
+        public void run() {
+                //切换ViewPager里的图片到下一个
+                int currentItem = loopPager.getCurrentItem();
+                loopPager.setCurrentItem(++currentItem, true);
+                handler.postDelayed(this, 1500);
+            }
+    };
+
+    private void initView(){
+        //找到viewPager的控件
+        loopPager=(ViewPager) this.findViewById(R.id.viewpager);
+        //设置适配器
+        loopPagerAdapter =new loopPagerAdapter();
+        loopPagerAdapter.setData(sPics);
+        loopPager.setAdapter(loopPagerAdapter);
+        loopPager.setCurrentItem(loopPagerAdapter.getDataRealSize()*100,false);
+    }
+ }
